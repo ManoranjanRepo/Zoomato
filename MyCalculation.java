8e055a57ca13ce80cd63100bf439208d8ab3f260
debashish <dev@gmail.com>
2021-11-19 21:26:39 +0530

    Commit no 2

---
+++ MyCalculation.java
@@ -0,0 +1,22 @@
+package com.xoriant.inheritance;
+
+public class MyCalculation extends Calculation {
+
+	public void multiplication(int x, int y) {
+	      z = x * y;
+	      System.out.println("The product of the given numbers:"+z);
+	   }
+		
+	   public static void main(String args[]) {
+	      int a = 20, b = 10;
+	      MyCalculation demo = new MyCalculation();
+	      demo.addition(a, b);
+	      demo.Subtraction(a, b);
+	      demo.multiplication(a, b);
+	      //super class ref veriable holding sub class object
+	      Calculation demo1 = new MyCalculation();
+	      demo.addition(a, b);
+	      demo.Subtraction(a, b);
+	   }
+
+}
